a5c4f672adbfe5280ce5b9a6bb31dd78d637b9e9
Suleyman Suleymanzade <sul_soft@hotmail.com>
2023-05-06 18:08:33 +0400

    pipeline

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# effective_xgboost" 
+"# effective_xgboost"
+**pycache**/
--- projects/p1/preprocess.py
+++ projects/p1/preprocess.py
@@ -7,10 +7,10 @@ def tweak_kag(df_: pd.DataFrame) -> pd.DataFrame:
     return (df_
             .assign(age=df_.Q2.str.slice(0, 2).astype(int),
                     education=df_.Q4.replace({
-                        "Master's degree": 18,
-                        "Bachelor's degree": 26,
+                        "Master’s degree": 18,
+                        "Bachelor’s degree": 16,
                         "Doctoral degree":20,
-                        "Some college/university study without earning a bachelor's degree": 13,
+                        "Some college/university study without earning a bachelor’s degree": 13,
                         "Professional degree": 19,
                         "I prefer not to answer": None,
                         "No formal education past high school": 12
@@ -18,9 +18,9 @@ def tweak_kag(df_: pd.DataFrame) -> pd.DataFrame:
                     major=(df_.Q5
                            .pipe(topn, n=3)
                            .replace({
-                               "Computer science (software engineering etc.)" : 'cs',
+                               "Computer science (software engineering, etc.)" : 'cs',
                                "Engineering (non-computer focused)" : 'eng',
-                               "Mathermatics or statistics": "stat"})
+                               "Mathematics or statistics": "stat"})
                            ),
                     years_exp = (df_.Q8.str.replace("+", "", regex=False)
                                  .str.split("-", expand=True)
@@ -48,3 +48,26 @@ def topn(ser, n=5, default="other"):
     counts = ser.value_counts()
     return ser.where(ser.isin(counts.index[:n]), default)
 
+
+class TweakKagTransformer(base.BaseEstimator, base.TransformerMixin):
+    def __init__(self, y_col=None):
+        self.y_col = y_col 
+
+    def transform(self, X):
+        return tweak_kag(X)
+    
+    def fit(self, X, y=None):
+        return self
+
+def get_rawX_y(df, y_col):
+    raw = (df
+           .query('Q3.isin(["United States of America", "China", "India"])'
+                  'and Q6.isin(["Data Scientist", "Software Engineer"])')
+           )
+    return raw.drop(columns=[y_col]), raw[y_col]
+
+kag_pl = pipeline.Pipeline(
+    [('tweak', TweakKagTransformer()),
+    ("cat", encoding.OneHotEncoder(top_categories=5, drop_last=True, variables=["Q1", "Q3", "major"])),
+    ("num_impute", imputation.MeanMedianImputer(imputation_method="median",variables=["education", "years_exp"]))
+     ])
